208eb9472398d216a0c1bffe79fd87e5b823a860
ERROR <95231870+zephyrXXX@users.noreply.github.com>
2022-05-12 15:48:22 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,39 @@
+function remov(){
+    document.getElementById("ebatCheck").checked = false;
+}
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+        
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            } 
+            
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_activeScroll');
+            } else {
+                animItem.classList.remove('_activeScroll');
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+
+    
+}
